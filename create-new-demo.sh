102e9c7606bb7e26d4a516b3838581c3f13abacb
making <makingx@gmail.com>
2016-11-05 04:48:36 +0900

    Update

--- create-new-demo.sh
+++ create-new-demo.sh
@@ -11,8 +11,25 @@ dir=`echo $title | tr  A-Z a-z | tr ' ' '-' | tr -d ',!/'`
 
 echo "create $dir"
 mkdir -p $dir
+
 echo "create $dir/$dir.log"
-touch $dir/$dir.log
+echo "stream create --name $dir --definition \"time | log\"" > $dir/$dir.log
+echo "stream deploy --name $dir" >> $dir/$dir.log
+
 echo "create $dir/README.md"
 echo "# $title" > $dir/README.md
+echo "" >> $dir/README.md
+echo "## Deploy stream" >> $dir/README.md
+echo "" >> $dir/README.md
+echo "\`\`\` shell" >> $dir/README.md
+echo "../deploy-stream.sh $dir.log" >> $dir/README.md
+echo "\`\`\`" >> $dir/README.md
+echo "" >> $dir/README.md
+echo "## Test stream" >> $dir/README.md
+echo "" >> $dir/README.md
+echo "## Destroy stream" >> $dir/README.md
+echo "" >> $dir/README.md
+echo "\`\`\` shell" >> $dir/README.md
+echo "../destroy-stream.sh $dir" >> $dir/README.md
+echo "\`\`\`" >> $dir/README.md
 echo "Done!"
